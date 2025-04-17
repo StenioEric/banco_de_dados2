1fd888a396bc40fac4ceb620e48577b7cbabb50c
StenioEric <stenioeric@gmail.com>
2025-04-17 16:00:00 -0300

    feat: questão 4 da lista #1

---
+++ tarefas/tarefa01-q04.sql
@@ -0,0 +1,7 @@
+SELECT f.nome, f.salario, d.descricao AS departamento
+FROM funcionario f
+JOIN departamento d ON f.cod_depto = d.codigo
+WHERE f.codigo NOT IN (
+    SELECT cod_gerente FROM departamento
+)
+ORDER BY d.codigo;
